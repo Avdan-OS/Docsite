17df62c4a022c9a05712a29d7d421a3205e4ec29
Rohith M <rohithmerala@gmail.com>
2023-01-08 13:03:02 +0530

    Added Apps Section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,35 +6,35 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'AvdanOS Documentation',
-  tagline: 'All the docs of AvdanOS',
-  url: 'https://avdanos.github.io/docs',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "AvdanOS Documentation",
+  tagline: "All the docs of AvdanOS",
+  url: "https://avdanos.github.io/docs",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Avdan-OS', // Usually your GitHub org/user name.
-  projectName: 'docsite', // Usually your repo name.
+  organizationName: "Avdan-OS", // Usually your GitHub org/user name.
+  projectName: "docsite", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebar/avdanos.js'),
-          path: "docs/avdanos"
+          sidebarPath: require.resolve("./sidebar/avdanos.js"),
+          path: "docs/avdanos",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
@@ -48,7 +48,7 @@ const config = {
           //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -56,32 +56,42 @@ const config = {
 
   plugins: [
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'nadva',
-        path: 'docs/nadva',
-        routeBasePath: 'nadva',
-        sidebarPath: require.resolve('./sidebar/nadva.js'),
+        id: "nadva",
+        path: "docs/nadva",
+        routeBasePath: "nadva",
+        sidebarPath: require.resolve("./sidebar/nadva.js"),
         // ... other options
       },
     ],
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'tinan',
-        path: 'docs/tinan',
-        routeBasePath: 'tinan',
-        sidebarPath: require.resolve('./sidebar/tinan.js'),
+        id: "tinan",
+        path: "docs/tinan",
+        routeBasePath: "tinan",
+        sidebarPath: require.resolve("./sidebar/tinan.js"),
         // ... other options
       },
     ],
     [
-      '@docusaurus/plugin-content-docs',
+      "@docusaurus/plugin-content-docs",
       {
-        id: 'demo',
-        path: 'docs/demo',
-        routeBasePath: 'demo',
-        sidebarPath: require.resolve('./sidebar/demo.js'),
+        id: "demo",
+        path: "docs/demo",
+        routeBasePath: "demo",
+        sidebarPath: require.resolve("./sidebar/demo.js"),
+        // ... other options
+      },
+    ],
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "apps",
+        path: "docs/apps",
+        routeBasePath: "apps",
+        sidebarPath: require.resolve("./sidebar/apps.js"),
         // ... other options
       },
     ],
@@ -91,124 +101,133 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
       },
       navbar: {
-        title: 'AvdanOS Docs',
+        title: "AvdanOS Docs",
         logo: {
-          alt: 'Avdan OS Docs Logo',
-          src: 'img/logo.svg',
+          alt: "Avdan OS Docs Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Project AvdanOS',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Project AvdanOS",
           },
           {
-            to: 'nadva/nadva-intro',
-            position: 'left',
-            label: 'Nadva',
+            to: "nadva/nadva-intro",
+            position: "left",
+            label: "Nadva",
           },
           {
-            to: 'tinan/tinan-overview',
-            position: 'left',
-            label: 'Tinan',
+            to: "tinan/tinan-overview",
+            position: "left",
+            label: "Tinan",
           },
           {
-            to: 'demo/demo-intro',
-            position: 'left',
-            label: 'Demo',
+            to: "demo/demo-intro",
+            position: "left",
+            label: "Demo",
           },
           {
-            href: 'https://github.com/Avdan-OS',
-            position: 'right',
-            className: 'header-github-link',
+            to: "apps/apps-intro",
+            position: "left",
+            label: "Apps",
           },
           {
-            type: 'localeDropdown',
-            position: 'right',
+            href: "https://github.com/Avdan-OS",
+            position: "right",
+            className: "header-github-link",
+          },
+          {
+            type: "localeDropdown",
+            position: "right",
             dropdownItemsAfter: [
               {
-                type: 'html',
+                type: "html",
                 value: '<hr style="margin: 0.3rem 0;">',
               },
               {
-                href: 'https://github.com/Avdan-OS/docsite',
-                label: 'Help Us Translate',
+                href: "https://github.com/Avdan-OS/docsite",
+                label: "Help Us Translate",
               },
             ],
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Overview',
-                to: '/docs/intro',
+                label: "Overview",
+                to: "/docs/intro",
               },
               {
-                label: 'Nadva',
-                to: '/nadva/nadva-intro',
+                label: "Nadva",
+                to: "/nadva/nadva-intro",
               },
               {
-                label: 'Tinan',
-                to: '/tinan/tinan-overview',
+                label: "Tinan",
+                to: "/tinan/tinan-overview",
               },
               {
-                label: 'Demo',
-                to: '/demo/demo-intro',
+                label: "Demo",
+                to: "/demo/demo-intro",
+              },
+              {
+                label: "Apps",
+                to: "/apps/apps-intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.gg/avdanos',
+                label: "Discord",
+                href: "https://discord.gg/avdanos",
               },
               {
-                label: 'YouTube',
-                href: 'https://www.youtube.com/channel/UCHLCBj83J7bR82HwjhCJusA',
+                label: "YouTube",
+                href: "https://www.youtube.com/channel/UCHLCBj83J7bR82HwjhCJusA",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/avdan_os',
+                label: "Twitter",
+                href: "https://twitter.com/avdan_os",
               },
               {
-                label: 'Instagram',
-                href: 'https://www.instagram.com/avdanos_/',
+                label: "Instagram",
+                href: "https://www.instagram.com/avdanos_/",
               },
               {
-                label: 'Reddit',
-                href: 'https://www.reddit.com/r/AvdanOS/',
+                label: "Reddit",
+                href: "https://www.reddit.com/r/AvdanOS/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               // {
               //   label: 'Blog',
               //   href: '/blog',
               // },
               {
-                label: 'Our Website',
-                href: 'https://avdanos.com/',
+                label: "Our Website",
+                href: "https://avdanos.com/",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/Avdan-OS/',
+                label: "GitHub",
+                href: "https://github.com/Avdan-OS/",
               },
               {
-                label: 'Download',
-                href: 'https://avdanos.com/downloads',
+                label: "Download",
+                href: "https://avdanos.com/downloads",
               },
             ],
           },
--- sidebars.js
+++ sidebars.js
@@ -1,13 +1,18 @@
 module.exports = {
-    mySidebar: [
-      // Normal syntax:
-      {
-        type: 'doc',
-        id: 'tinan', // document ID
-        label: 'Tinan', // sidebar label
-      },
-  
-      // Shorthand syntax:
-      'doc2', // document ID
-    ],
-  };
+  mySidebar: [
+    // Normal syntax:
+    {
+      type: "doc",
+      id: "tinan", // document ID
+      label: "Tinan", // sidebar label
+    },
+    {
+      type: "doc",
+      id: "apps", // document ID
+      label: "Apps", // sidebar label
+    },
+
+    // Shorthand syntax:
+    "doc2", // document ID
+  ],
+};
